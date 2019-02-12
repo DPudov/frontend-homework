920466555610a48d317d7ab81407921b13a69db5
Dmitry Pudov <dimapudov99@gmail.com>
2019-02-12 17:22:58 +0300

    Condition is simplified

--- source/sorting.js
+++ source/sorting.js
@@ -20,13 +20,23 @@ function orderMultipleFields(left, right, props) {
 	return equality;
 }
 
-const sorting = function(array, properties) {
-	if (!array) return [];
+function isWrongArray(array) {
+	return (!array || !Array.isArray(array));
+}
 
-	if (!properties ||
-		array.length === 0 ||
-		array.length === 1 ||
-		properties.length === 0) return array;
+function isWrongLength(array, properties) {
+	const lenArray = array.length;
+	return (lenArray === 0 || lenArray === 1 || properties.length === 0);
+}
+
+const sorting = function(array, properties) {
+	if (isWrongArray(array)) {
+		return [];
+	}
+
+	if (isWrongArray(properties) || isWrongLength(array, properties)) {
+		return array;
+	}
 
 	return array.sort(function(a, b) { return orderMultipleFields(a, b, properties); });
 };
