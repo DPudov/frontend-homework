51ccb1e06eb2e5cca6d46a02771caa76a54051c5
Dmitry Pudov <dimapudov99@gmail.com>
2019-02-14 17:25:30 +0300

    Arrow functions

--- source/sorting.js
+++ source/sorting.js
@@ -1,42 +1,39 @@
 'use strict';
 
-function orderMultipleFields(left, right, props) {
+const isWrongArray = (array) => !Array.isArray(array);
+
+const needSorting = (array, properties) => {
+	const lenArray = array.length;
+	return !(lenArray === 0 || lenArray === 1 || properties.length === 0);
+}
+
+const isString = (element) => typeof element === 'string';
+
+const orderMultipleFields = (left, right, props) => {
 	let equality = 0;
+	props.every((element) => {
+		const leftProp = left[element];
+		const rightProp = right[element];
+		equality = leftProp - rightProp;
 
-	props.every(function(element) {
-		equality = left[element] - right[element];
-
-		if (typeof left[element] === 'string' || typeof right[element] === 'string') {
-			equality = left[element].localeCompare(right[element]);
+		if (isString(leftProp) || isString(rightProp)) {
+			equality = leftProp.localeCompare(rightProp);
 		}
 
-		if (equality !== 0) {
-			return false;
-		}
-
-		return true;
-	})
+		return equality === 0;
+	});
 
 	return equality;
 }
 
-function isWrongArray(array) {
-	return (!array || !Array.isArray(array));
-}
-
-function needSorting(array, properties) {
-	const lenArray = array.length;
-	return (lenArray === 0 || lenArray === 1 || properties.length === 0);
-}
-
-const sorting = function(array, properties) {
+const sorting = (array, properties) => {
 	if (isWrongArray(array)) {
 		return [];
 	}
 
-	if (isWrongArray(properties) || needSorting(array, properties)) {
+	if (isWrongArray(properties) || !needSorting(array, properties)) {
 		return array;
 	}
 
-	return array.sort(function(a, b) { return orderMultipleFields(a, b, properties); });
+	return array.sort((a, b) => orderMultipleFields(a, b, properties));
 };
